4c335596186b924b16d8b4e3bea8cf8fee2a7aae
Allan Gadelha <allangadelha@gmail.com>
2022-06-09 17:00:42 -0300

    Route profile

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
-const profiles = require("./src/profiles/profiles.json")
+const profiles = require("./profiles.json")
 app.get("/profiles", (req, res) => {
     return res.json(profiles);
+})
+
+app.get("/profiles/:id", (req, res) => {
+    let id = req.params.id;
+    return res.json([profiles[id]])
 })
 
 app.listen(port, () => console.log("Conectado com sucesso"));
